db6f84d8e0f17a5e80d3c36e8d4e6ce17791b087
Jamie Dawson <jadawson@e1z3r2p11.42.us.org>
2019-10-08 16:46:27 -0700

    added links to regional news

--- code/feeds.js
+++ code/feeds.js
@@ -4,15 +4,123 @@
 // Tags: What the user can say to automatically call certain feeds
 module.exports = module.exports = [
   {
-    url: 'https://thefantasyfootballers.libsyn.com/fantasyfootball',
-    urlText: 'Click here to see the full story on thefantasyfootballers.libsyn.com',
-    text: 'Example Podcast',
-    tags: ['the fantasy footballers']
+    url: 'https://rss.cbc.ca/lineup/canada-britishcolumbia.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'British Columbia',
+    tags: ['british columbia'] //
   },
   {
-    url: 'https://www.ketoconnect.net/feed/',
-    urlText: 'Click here to see the full story on espn.com',
-    text: 'Example Text',
-    tags: ['sports', 'espn']
+    url: 'https://rss.cbc.ca/lineup/canada-kamloops.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Kamloops',
+    tags: ['kamloops'] //
+  },
+  {
+    url: 'https://rss.cbc.ca/lineup/canada-calgary.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Calgary',
+    tags: ['calgary'] //
+  },
+    {
+    url: 'https://rss.cbc.ca/lineup/canada-edmonton.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Edmonton',
+    tags: ['edmonton'] //
+  },
+    {
+    url: 'https://rss.cbc.ca/lineup/canada-saskatchewan.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Saskatchewan',
+    tags: ['Saskatchewan']//
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-saskatoon.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Saskatoon',
+    tags: ['saskatoon'] //
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-manitoba.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Manitoba',
+    tags: ['manitoba'] //
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-thunderbay.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Thunderbay',
+    tags: ['thunderbay'] //
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-sudbury.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Sudbury',
+    tags: ['sudbury'] //
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-windsor.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Windsor',
+    tags: ['windsor']
+  },
+     {
+    url: 'https://www.cbc.ca/cmlink/rss-canada-london',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'London',
+    tags: ['London'] //
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-kitchenerwaterloo.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Kitchener-Waterloo',
+    tags: ['Kitchener-Waterloo', 'kitchener', 'waterloo', 'kitchenerwaterloo']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-toronto.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Toronto',
+    tags: ['toronto']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-hamiltonnews.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Hamilton',
+    tags: ['hamilton']
+  },
+     {
+    url: 'http://rss.cbc.ca/lineup/canada-montreal.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Montreal',
+    tags: ['montreal']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-newbrunswick.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'New Brunswick',
+    tags: ['New Brunswick', 'newbrunswick', 'new-brunswick', 'brunswick']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-pei.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Prince Edward Island',
+    tags: ['prince edward island', 'edward island']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-novascotia.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Nova Scotia',
+    tags: ['nova scotia', 'scotia', 'nova-scotia', 'nova']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-newfoundland.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'Newfoundland & Labrador',
+    tags: ['newfoundland', 'labrador', 'new found land', 'new foundland']
+  },
+     {
+    url: 'https://rss.cbc.ca/lineup/canada-north.xml',
+    urlText: 'Click here to see the full story on cbc.com',
+    text: 'North',
+    tags: ['north']
   },
 ]
